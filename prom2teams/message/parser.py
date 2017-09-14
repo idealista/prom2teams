37783a682b97f1f5967a91c224d89f9e142f1bc9
Jose A. Noguera <janoguera@idealista.com>
2017-09-14 14:27:21 +0200

    #21

--- prom2teams/message/parser.py
+++ prom2teams/message/parser.py
@@ -6,19 +6,28 @@ logger = logging.getLogger()
 
 
 def parse(json_str):
-    logger.debug('received: %s', json_str)
-
+    return_dict = {}
     json_values = json.loads(json_str)
 
     json_alerts_attr = json_values['alerts'][0]
     json_alerts_labels_attr = json_alerts_attr['labels']
     json_alerts_annotations_attr = json_alerts_attr['annotations']
 
-    return {
-        'alert_name': json_alerts_labels_attr['alertname'],
-        'alert_instance': json_alerts_labels_attr['instance'],
-        'alert_severity': json_alerts_labels_attr['severity'],
-        'alert_summary': json_alerts_annotations_attr['summary'],
-        'alert_description': json_alerts_annotations_attr['description'],
-        'alert_status': json_alerts_attr['status']
-    }
+    mandatory_fields = ['alertname', 'status', 'instance', 'summary']
+    optional_fields = ['severity', 'description']
+    fields = mandatory_fields + optional_fields
+
+    for field in fields:
+        if field in json_alerts_attr:
+            return_dict['alert_'+field] = json_alerts_attr[field]
+        elif field in json_alerts_labels_attr:
+            return_dict['alert_'+field] = json_alerts_labels_attr[field]
+        elif field in json_alerts_annotations_attr:
+            return_dict['alert_'+field] = json_alerts_annotations_attr[field]
+        elif field in mandatory_fields:
+            return_dict['alert_severity'] = 'severe'
+            return_dict['alert_summary'] = 'Incorrect JSON received. At least one mandatory field ('+field+') is absent.'
+            return_dict['alert_status'] = 'incorrect'            
+            return return_dict
+
+    return return_dict
