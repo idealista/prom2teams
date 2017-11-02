6c8e92defd8a3ba7b5dd51a19805592ffc30a5e0
Jose <janoguera@idealista.com>
2017-11-02 12:52:11 +0100

    #21 Modified parser and template e

--- prom2teams/message/parser.py
+++ prom2teams/message/parser.py
@@ -4,18 +4,12 @@ import logging
 
 logger = logging.getLogger()
 
-
-def parse(json_str):
-    alert_fields = {}
-    json_values = json.loads(json_str)
-
-    json_alerts_attr = json_values['alerts'][0]
-    json_alerts_labels_attr = json_alerts_attr['labels']
-    json_alerts_annotations_attr = json_alerts_attr['annotations']
-
+def check_fields(json_alerts_attr, json_alerts_labels_attr, json_alerts_annotations_attr):
     mandatory_fields = ['alertname', 'status', 'instance', 'summary']
     optional_fields = ['severity', 'description']
     fields = mandatory_fields + optional_fields
+
+    alert_fields = {}
 
     for field in fields:
         alert_field_key = 'alert_' + field
@@ -27,8 +21,17 @@ def parse(json_str):
             alert_fields[alert_field_key] = json_alerts_annotations_attr[field]
         elif field in mandatory_fields:
             alert_fields['alert_severity'] = 'severe'
-            alert_fields['alert_summary'] = 'Incorrect JSON received. At least one mandatory field ('+field+') is absent.'
             alert_fields['alert_status'] = 'incorrect'
+            alert_fields['alert_summary'] = 'Incorrect JSON received. At least one mandatory field ('+field+') is absent.'
             return alert_fields
 
     return alert_fields
+
+def parse(json_str):
+    json_values = json.loads(json_str)
+
+    json_alerts_attr = json_values['alerts'][0]
+    json_alerts_labels_attr = json_alerts_attr['labels']
+    json_alerts_annotations_attr = json_alerts_attr['annotations']
+
+    return check_fields(json_alerts_attr, json_alerts_labels_attr, json_alerts_annotations_attr)
