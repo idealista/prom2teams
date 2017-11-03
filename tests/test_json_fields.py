7634880e1d3b815694d2d710c546f999c9c59890
Jose <janoguera@idealista.com>
2017-11-03 11:57:32 +0100

    #21 Variable TEST_CONFIG_FILES_PATH update

--- tests/test_json_fields.py
+++ tests/test_json_fields.py
@@ -6,22 +6,22 @@ from context import parse
 
 class TestServer(unittest.TestCase):
 
-    TEST_CONFIG_FILES_PATH = 'tests/data/'
+    TEST_CONFIG_FILES_PATH = 'tests/data/jsons'
 
     def test_json_with_all_fields(self):
-        with open(self.TEST_CONFIG_FILES_PATH + 'jsons/all_ok.json') as json_data:
+        with open(self.TEST_CONFIG_FILES_PATH + 'all_ok.json') as json_data:
             json_received = json.load(json_data)
             alert_fields = parse(json.dumps(json_received))
             self.assertNotIn('Incorrect',str(alert_fields))
 
     def test_json_without_mandatory_field(self):
-        with open(self.TEST_CONFIG_FILES_PATH + 'jsons/without_mandatory_field.json') as json_data:
+        with open(self.TEST_CONFIG_FILES_PATH + 'without_mandatory_field.json') as json_data:
             json_received = json.load(json_data)
             alert_fields = parse(json.dumps(json_received))
             self.assertIn('Incorrect',str(alert_fields))
 
     def test_json_without_optional_field(self):
-        with open(self.TEST_CONFIG_FILES_PATH + 'jsons/without_optional_field.json') as json_data:
+        with open(self.TEST_CONFIG_FILES_PATH + 'without_optional_field.json') as json_data:
             json_received = json.load(json_data)
             alert_fields = parse(json.dumps(json_received))
             self.assertNotIn('Incorrect',str(alert_fields))
