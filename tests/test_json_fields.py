450fdff93e36b99831eeb097910a32bbb9f28613
Manu Hortet <manuhortet@gmail.com>
2018-08-08 11:18:23 +0200

    Test related to grouping alerts added

--- tests/test_json_fields.py
+++ tests/test_json_fields.py
@@ -50,6 +50,18 @@ class TestJSONFields(unittest.TestCase):
 
                 self.assertDictEqual(json_rendered, json_expected)
 
+    def test_grouping_multiple_alerts(self):
+        with open(self.TEST_CONFIG_FILES_PATH + 'all_ok_multiple.json') as json_data:
+            with open(self.TEST_CONFIG_FILES_PATH + 'teams_alarm_all_ok_multiple.json') as expected_data:
+                json_received = json.load(json_data)
+                json_expected = json.load(expected_data)
+
+                alerts = MessageSchema().load(json_received).data
+                rendered_data = AlarmSender(group_alerts='TRUE')._create_alarms(alerts)[0]
+                json_rendered = json.loads(rendered_data)
+
+                self.assertDictEqual(json_rendered, json_expected)
+
 
 if __name__ == '__main__':
     unittest.main()
