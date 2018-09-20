daad39cab1f485bd5050cecd303a291c53db0a3b
Manu Hortet <manuhortet@gmail.com>
2018-09-20 09:28:43 +0200

    always group by status

--- prom2teams/teams/alarm_mapper.py
+++ prom2teams/teams/alarm_mapper.py
@@ -9,31 +9,35 @@ def map_alarm_to_json(alarm):
 
 
 def map_prom_alerts_to_teams_alarms(alerts):
+    alerts = group_alerts(alerts, 'status')
     teams_alarms = []
     schema = TeamsAlarmSchema()
-    for alert in alerts:
-        alarm = TeamsAlarm(alert.name, alert.status.lower(), alert.severity,
-                           alert.summary, alert.instance, alert.description)
-        json_alarm = schema.dump(alarm).data
-        teams_alarms.append(json_alarm)
+    for same_status_alerts in alerts:
+        for alert in alerts[same_status_alerts]:
+            alarm = TeamsAlarm(alert.name, alert.status.lower(), alert.severity,
+                               alert.summary, alert.instance, alert.description)
+            json_alarm = schema.dump(alarm).data
+            teams_alarms.append(json_alarm)
     return teams_alarms
 
 
 def map_and_group(alerts, group_alerts_by):
+    alerts = group_alerts(alerts, 'status')
     teams_alarms = []
     schema = TeamsAlarmSchema()
-    grouped_alerts = group_alerts(alerts, group_alerts_by)
-    for alert in grouped_alerts:
-        features = group_features(grouped_alerts[alert])
-        name, description, instance, severity, status, summary = (teams_visualization(features["name"]),
-                                                                  teams_visualization(features["description"]),
-                                                                  teams_visualization(features["instance"]),
-                                                                  teams_visualization(features["severity"]),
-                                                                  teams_visualization(features["status"]),
-                                                                  teams_visualization(features["summary"]))
-        alarm = TeamsAlarm(name, status.lower(), severity, summary, instance, description)
-        json_alarm = schema.dump(alarm).data
-        teams_alarms.append(json_alarm)
+    for same_status_alerts in alerts:
+        grouped_alerts = group_alerts(alerts[same_status_alerts], group_alerts_by)
+        for alert in grouped_alerts:
+            features = group_features(grouped_alerts[alert])
+            name, description, instance, severity, status, summary = (teams_visualization(features["name"]),
+                                                                      teams_visualization(features["description"]),
+                                                                      teams_visualization(features["instance"]),
+                                                                      teams_visualization(features["severity"]),
+                                                                      teams_visualization(features["status"]),
+                                                                      teams_visualization(features["summary"]))
+            alarm = TeamsAlarm(name, status.lower(), severity, summary, instance, description)
+            json_alarm = schema.dump(alarm).data
+            teams_alarms.append(json_alarm)
     return teams_alarms
 
 
