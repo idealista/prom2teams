3f448d2eaa9be121372d9ff343aee1fe01f6a31c
Daniel Ortega <dortega@idealista.com>
2020-04-12 19:30:21 +0200

    #177 Fixing unit tests when using Python <= 3.6

--- tests/test_app_configuration.py
+++ tests/test_app_configuration.py
@@ -1,4 +1,6 @@
 import unittest
+import os
+
 from prom2teams.app import configuration
 from prom2teams.app import exceptions
 
@@ -8,23 +10,23 @@ class TestServer(unittest.TestCase):
     DEFAULT_CONFIG_RELATIVE_PATH = './prom2teams/config.ini'
 
     def test_get_config_with_invalid_path(self):
-        invalid_relative_path = self.TEST_CONFIG_FILES_PATH + 'invalid_path'
+        invalid_relative_path = os.path.join(self.TEST_CONFIG_FILES_PATH, 'invalid_path')
         self.assertRaises(FileNotFoundError, configuration._config_provided, invalid_relative_path)
 
     def test_get_config_without_required_keys_should_raise_exception(self):
-        empty_config_relative_path = self.TEST_CONFIG_FILES_PATH + 'empty_config.ini'
+        empty_config_relative_path = os.path.join(self.TEST_CONFIG_FILES_PATH, 'empty_config.ini')
 
         self.assertRaises(exceptions.MissingConnectorConfigKeyException, configuration._config_provided,
                           empty_config_relative_path)
 
     def test_get_config_without_override(self):
-        provided_config_relative_path = self.TEST_CONFIG_FILES_PATH + 'without_overriding_defaults.ini'
+        provided_config_relative_path = os.path.join(self.TEST_CONFIG_FILES_PATH, 'not_overriding_defaults.ini')
         config = configuration._config_provided(provided_config_relative_path)
 
         self.assertTrue(config.get('Microsoft Teams', 'Connector'))
 
     def test_get_config_overriding_defaults(self):
-        provided_config_relative_path = self.TEST_CONFIG_FILES_PATH + 'overriding_defaults.ini'
+        provided_config_relative_path = os.path.join(self.TEST_CONFIG_FILES_PATH, 'overriding_defaults.ini')
         config = configuration._config_provided(provided_config_relative_path)
 
         self.assertEqual(config.get('HTTP Server', 'Host'), '1.1.1.1')
@@ -32,7 +34,7 @@ class TestServer(unittest.TestCase):
         self.assertTrue(config.get('Microsoft Teams', 'Connector'))
 
     def test_connectors_configured(self):
-        provided_config_relative_path = self.TEST_CONFIG_FILES_PATH + 'multiple_connectors_config.ini'
+        provided_config_relative_path = os.path.join(self.TEST_CONFIG_FILES_PATH, 'multiple_connectors_config.ini')
         config = configuration._config_provided(provided_config_relative_path)
 
         self.assertEqual(config['Microsoft Teams']['connector1'], 'teams_webhook_url')
@@ -40,7 +42,7 @@ class TestServer(unittest.TestCase):
         self.assertEqual(config['Microsoft Teams']['connector3'], 'definitely_another_teams_webhook_url')
 
     def test_get_config_for_all_fields(self):
-        provided_config_relative_path = self.TEST_CONFIG_FILES_PATH + 'all_fields.ini'
+        provided_config_relative_path = os.path.join(self.TEST_CONFIG_FILES_PATH, 'all_fields.ini')
         config = configuration._config_provided(provided_config_relative_path)
 
         self.assertEqual(config.get('HTTP Server', 'Host'), '1.1.1.1')
@@ -50,8 +52,6 @@ class TestServer(unittest.TestCase):
         self.assertEqual(config.get('Log', 'Path'), '/var/log/prom2teams/test.log')
         self.assertEqual(config.get('Template', 'Path'), 'jinja2/template/path')
         self.assertEqual(config.get('Group Alerts', 'Field'), 'name')
-
-
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_json_fields.py
+++ tests/test_json_fields.py
@@ -1,45 +1,45 @@
 import unittest
+import os
 import json
 
 from prom2teams.teams.alarm_mapper import map_prom_alerts_to_teams_alarms
 from prom2teams.prometheus.message_schema import MessageSchema
 from prom2teams.app.sender import AlarmSender
 
-
 class TestJSONFields(unittest.TestCase):
-    TEST_CONFIG_FILES_PATH = 'tests/data/jsons/'
+    TEST_CONFIG_FILES_PATH = 'tests/data/json_files/'
 
     def test_json_with_all_fields(self):
-        with open(self.TEST_CONFIG_FILES_PATH + 'all_ok.json') as json_data:
+        with open(os.path.join(self.TEST_CONFIG_FILES_PATH, 'all_ok.json')) as json_data:
             json_received = json.load(json_data)
             alerts = MessageSchema().load(json_received)
             alarm = map_prom_alerts_to_teams_alarms(alerts)[0]
             self.assertNotIn('unknown', str(alarm))
 
     def test_json_without_mandatory_field(self):
-        with open(self.TEST_CONFIG_FILES_PATH + 'without_mandatory_field.json') as json_data:
+        with open(os.path.join(self.TEST_CONFIG_FILES_PATH, 'without_mandatory_field.json')) as json_data:
             json_received = json.load(json_data)
             alerts = MessageSchema().load(json_received)
             alarm = map_prom_alerts_to_teams_alarms(alerts)[0]
             self.assertIn('unknown', str(alarm))
 
     def test_json_without_optional_field(self):
-        with open(self.TEST_CONFIG_FILES_PATH + 'without_optional_field.json') as json_data:
+        with open(os.path.join(self.TEST_CONFIG_FILES_PATH, 'without_optional_field.json')) as json_data:
             json_received = json.load(json_data)
             alerts = MessageSchema().load(json_received)
             alarm = map_prom_alerts_to_teams_alarms(alerts)[0]
             self.assertIn("'description': 'unknown'", str(alarm))
 
     def test_json_without_instance_field(self):
-        with open(self.TEST_CONFIG_FILES_PATH + 'without_instance_field.json') as json_data:
+        with open(os.path.join(self.TEST_CONFIG_FILES_PATH, 'without_instance_field.json')) as json_data:
             json_received = json.load(json_data)
             alerts = MessageSchema().load(json_received)
             alarm = map_prom_alerts_to_teams_alarms(alerts)[0]
             self.assertEqual('unknown', str(alarm['instance']))
 
     def test_compose_all(self):
-        with open(self.TEST_CONFIG_FILES_PATH + 'all_ok.json') as json_data:
-            with open(self.TEST_CONFIG_FILES_PATH + 'teams_alarm_all_ok.json') as expected_data:
+        with open(os.path.join(self.TEST_CONFIG_FILES_PATH, 'all_ok.json')) as json_data:
+            with open(os.path.join(self.TEST_CONFIG_FILES_PATH, 'teams_alarm_all_ok.json')) as expected_data:
                 json_received = json.load(json_data)
                 json_expected = json.load(expected_data)
 
@@ -51,8 +51,8 @@ class TestJSONFields(unittest.TestCase):
 
     def test_with_common_items(self):
         self.maxDiff = None
-        with open(self.TEST_CONFIG_FILES_PATH + 'with_common_items.json') as json_data:
-            with open(self.TEST_CONFIG_FILES_PATH + 'teams_alarm_with_common_items.json') as expected_data:
+        with open(os.path.join(self.TEST_CONFIG_FILES_PATH, 'with_common_items.json')) as json_data:
+            with open(os.path.join(self.TEST_CONFIG_FILES_PATH, 'teams_alarm_with_common_items.json')) as expected_data:
                 json_received = json.load(json_data)
                 json_expected = json.load(expected_data)
 
@@ -63,8 +63,8 @@ class TestJSONFields(unittest.TestCase):
                 self.assertEqual(json_rendered.keys(), json_expected.keys())
 
     def test_grouping_multiple_alerts(self):
-        with open(self.TEST_CONFIG_FILES_PATH + 'all_ok_multiple.json') as json_data:
-            with open(self.TEST_CONFIG_FILES_PATH + 'teams_alarm_all_ok_multiple.json') as expected_data:
+        with open(os.path.join(self.TEST_CONFIG_FILES_PATH, 'all_ok_multiple.json')) as json_data:
+            with open(os.path.join(self.TEST_CONFIG_FILES_PATH, 'teams_alarm_all_ok_multiple.json')) as expected_data:
                 json_received = json.load(json_data)
                 json_expected = json.load(expected_data)
 
@@ -72,12 +72,12 @@ class TestJSONFields(unittest.TestCase):
                 rendered_data = AlarmSender(group_alerts_by='name')._create_alarms(alerts)[0].replace("\n\n\n", " ")
                 json_rendered = json.loads(rendered_data)
 
-                self.assertDictEqual(json_rendered, json_expected)
+                self.assertEqual(json.dumps(json_rendered, sort_keys=True), json.dumps(json_expected, sort_keys=True))
 
     def test_with_extra_labels(self):
         excluded_labels = ('pod_name', )
-        with open(self.TEST_CONFIG_FILES_PATH + 'all_ok_extra_labels.json') as json_data:
-            with open(self.TEST_CONFIG_FILES_PATH + 'teams_alarm_all_ok_extra_labels.json') as expected_data:
+        with open(os.path.join(self.TEST_CONFIG_FILES_PATH, 'all_ok_extra_labels.json')) as json_data:
+            with open(os.path.join(self.TEST_CONFIG_FILES_PATH, 'teams_alarm_all_ok_extra_labels.json')) as expected_data:
                 json_received = json.load(json_data)
                 json_expected = json.load(expected_data)
 
@@ -85,12 +85,12 @@ class TestJSONFields(unittest.TestCase):
                 rendered_data = AlarmSender()._create_alarms(alerts)[0]
                 json_rendered = json.loads(rendered_data)
 
-                self.assertDictEqual(json_rendered, json_expected)
+                self.assertEqual(json.dumps(json_rendered, sort_keys=True), json.dumps(json_expected, sort_keys=True))
 
     def test_with_extra_annotations(self):
         excluded_annotations = ('message', )
-        with open(self.TEST_CONFIG_FILES_PATH + 'all_ok_extra_annotations.json') as json_data:
-            with open(self.TEST_CONFIG_FILES_PATH + 'teams_alarm_all_ok_extra_annotations.json') as expected_data:
+        with open(os.path.join(self.TEST_CONFIG_FILES_PATH, 'all_ok_extra_annotations.json')) as json_data:
+            with open(os.path.join(self.TEST_CONFIG_FILES_PATH, 'teams_alarm_all_ok_extra_annotations.json')) as expected_data:
                json_received = json.load(json_data)
                json_expected = json.load(expected_data)
 
@@ -98,8 +98,7 @@ class TestJSONFields(unittest.TestCase):
                rendered_data = AlarmSender()._create_alarms(alerts)[0]
                json_rendered = json.loads(rendered_data)
 
-               self.assertDictEqual(json_rendered, json_expected)
-
+               self.assertEqual(json.dumps(json_rendered, sort_keys=True), json.dumps(json_expected, sort_keys=True))
 
 if __name__ == '__main__':
     unittest.main()
