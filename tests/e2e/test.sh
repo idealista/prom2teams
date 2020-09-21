e514b155d731ef132f2e2529d0fd4ae7e82136c3
Pablo Gómez-Caldito <pgomezcaldito@idealista.com>
2020-09-21 09:28:23 +0200

    #213 Create expectations mockserver

--- tests/e2e/test.sh
+++ tests/e2e/test.sh
@@ -1,106 +1,110 @@
 #!/bin/bash
 
-# Alertmanager 0.21.0
-# {
-#  "receiver": "test",
-#  "status": "firing",
-#  "alerts": [
-#    {
-#      "status": "firing",
-#      "labels": {
-#        "alertname": "NodeExporter",
-#        "env": "production",
-#        "instance": "node1.summit",
-#        "job": "node_exporter",
-#        "notify_room": "test",
-#        "severity": "critical",
-#        "type": "nodeexporter"
-#      },
-#      "annotations": {
-#        "description": "Node exporter down on node1.summit",
-#        "summary": "Node exporter down on node1.summit"
-#      },
-#      "startsAt": "2020-09-16T07:38:01.586706006Z",
-#      "endsAt": "0001-01-01T00:00:00Z",
-#      "generatorURL": "",
-#      "fingerprint": "e4ad109767ee663e"
-#    }
-#  ],
-#  "groupLabels": {
-#    "alertname": "NodeExporter"
-#  },
-#  "commonLabels": {
-#    "alertname": "NodeExporter",
-#    "job": "node_exporter",
-#    "severity": "critical",
-#    "type": "nodeexporter"
-#  },
-#  "commonAnnotations": {},
-#  "externalURL": "http://localhost:9093",
-#  "version": "4",
-#  "groupKey": "{}/{notify_room=~\"^(?:.*test.*)$\"}:{alertname=\"NodeExporter\"}",
-#  "truncatedAlerts": 0
-#}
-#  
-
-# Alertmanager 0.20.0
-# {
-#  "receiver": "test",
-#  "status": "firing",
-#  "alerts": [
-#    {
-#      "status": "firing",
-#      "labels": {
-#        "alertname": "NodeExporter",
-#        "env": "production",
-#        "instance": "node2.summit",
-#        "job": "node_exporter",
-#        "notify_room": "test",
-#        "severity": "critical",
-#        "type": "nodeexporter"
-#      },
-#      "annotations": {
-#        "description": "Node exporter down on node2.summit",
-#        "summary": "Node exporter down on node2.summit"
-#      },
-#      "startsAt": "2020-09-16T08:09:50.791949454Z",
-#      "endsAt": "0001-01-01T00:00:00Z",
-#      "generatorURL": "generator",
-#      "fingerprint": "96b211b05dc430e8"
-#    }
-#  ],
-#  "groupLabels": {
-#    "alertname": "NodeExporter"
-#  },
-#  "commonLabels": {
-#    "alertname": "NodeExporter",
-#    "env": "production",
-#    "job": "node_exporter",
-#    "notify_room": "test",
-#    "severity": "critical",
-#    "type": "nodeexporter"
-#  },
-#  "commonAnnotations": {},
-#  "externalURL": "http://localhost:9093",
-#  "version": "4",
-#  "groupKey": "{}/{notify_room=~\"^(?:.*test.*)$\"}:{alertname=\"NodeExporter\"}"
-#}
-
 echo "Creating containers..."
 docker-compose up --build -d > /dev/null 2>&1
 
-echo "Creating expectations and responses..."
+echo "Creating expectations and responses in teams mockserver..."
 sleep 10
-curl -v -X PUT "http://localhost:1080/mockserver/expectation" -d '{
+curl -s -v -X PUT "http://localhost:1080/mockserver/expectation" -d '{
   "httpRequest" : {
     "method" : "POST",
-    "path" : "/api/test"
+    "path" : "/api/test",
+    "body": {
+    "@type": "MessageCard",
+    "@context": "http://schema.org/extensions",
+    "themeColor": " 8C1A1A ",
+    "summary": "Node exporter down on node1.summit",
+    "title": "Prometheus alarm ",
+    "sections": [{
+        "activityTitle": "Node exporter down on node1.summit",
+        "facts": [{
+            "name": "Alarm",
+            "value": "NodeExporter"
+        },{
+            "name": "In host",
+            "value": "node1.summit"
+        },{
+            "name": "Severity",
+            "value": "critical"
+        },{
+            "name": "Description",
+            "value": "Node exporter down on node1.summit"
+        },{
+            "name": "Status",
+            "value": "firing"
+        },{
+            "name": "job",
+            "value": "node_exporter"
+        },{
+            "name": "env",
+            "value": "production"
+        },{
+            "name": "notify_room",
+            "value": "test"
+        },{
+            "name": "type",
+            "value": "nodeexporter"
+        }        ],
+          "markdown": true
+    }]
+}
   },
   "httpResponse" : {
     "statusCode": 200,
     "body" : "1"
   }
-}'
+}' > /dev/null 2>&1
+
+
+curl -s -v -X PUT "http://localhost:1080/mockserver/expectation" -d '{
+  "httpRequest" : {
+    "method" : "POST",
+    "path" : "/api/test",
+    "body": {
+    "@type": "MessageCard",
+    "@context": "http://schema.org/extensions",
+    "themeColor": " 8C1A1A ",
+    "summary": "Node exporter down on node2.summit",
+    "title": "Prometheus alarm ",
+    "sections": [{
+        "activityTitle": "Node exporter down on node2.summit",
+        "facts": [{
+            "name": "Alarm",
+            "value": "NodeExporter"
+        },{
+            "name": "In host",
+            "value": "node2.summit"
+        },{
+            "name": "Severity",
+            "value": "critical"
+        },{
+            "name": "Description",
+            "value": "Node exporter down on node2.summit"
+        },{
+            "name": "Status",
+            "value": "firing"
+        },{
+            "name": "job",
+            "value": "node_exporter"
+        },{
+            "name": "env",
+            "value": "production"
+        },{
+            "name": "notify_room",
+            "value": "test"
+        },{
+            "name": "type",
+            "value": "nodeexporter"
+        }        ],
+          "markdown": true
+    }]
+}
+  },
+  "httpResponse" : {
+    "statusCode": 200,
+    "body" : "1"
+  }
+}' > /dev/null 2>&1
 
 
 echo "Running tests..."
