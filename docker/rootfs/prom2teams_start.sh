ff08be3e415e46cfae0d2dd40f672e19a02daa81
Santiago Perez <sperez@idealista.com>
2022-12-07 15:53:05 +0100

    #307 fix typo

--- docker/rootfs/prom2teams_start.sh
+++ docker/rootfs/prom2teams_start.sh
@@ -5,7 +5,7 @@ if [ ! -f "/opt/prom2teams/config.ini" ]; then
     mv /opt/prom2teams/config.ini.tmp /opt/prom2teams/config.ini
 fi
 
-if [[ ! -f "/opt/prom2teams/uwsgi.ini" ]]; then
+if [ ! -f "/opt/prom2teams/uwsgi.ini" ]; then
     mv /opt/prom2teams/uwsgi.ini.tmp /opt/prom2teams/uwsgi.ini
 fi
 
