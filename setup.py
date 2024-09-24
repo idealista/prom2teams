4a12f96e9c84e44626fd55fa134e4e710402cb89
Santiago Perez <sperez@idealista.com>
2024-09-24 17:41:16 +0200

    Update version in release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md', encoding='utf-8') as f:
 
 
 setup(name='prom2teams',
-      version='4.2.0',
+      version='5.0.0',
       description='Project that redirects Prometheus Alert Manager '
       'notifications to Microsoft Teams',
       long_description=readme,
